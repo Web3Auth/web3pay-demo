34ceda4a9bdfb6027bc28a4358c5d470c96e1265
lwin <lwin@tor.us>
2024-08-21 16:06:35 +0800

    update lock file

---
+++ src/components/ConnectStep.tsx
@@ -0,0 +1,237 @@
+import { useWallet } from "@/context/walletContext";
+import { sliceAddress, openInNewTab } from "@/utils";
+import { IRandomWallet, ConnectWeb3PayStep } from "@/utils/interfaces";
+import { calculateBaseUrl, cn } from "@/utils/utils";
+import { generatePrivate, getPublic } from "@toruslabs/eccrypto";
+import Image from "next/image";
+import { useState } from "react";
+import { createPublicClient, Hex, http } from "viem";
+import { privateKeyToAddress } from "viem/accounts";
+import ImportFlowCard from "./ui/ImportFlowCard";
+import ErrorPopup from "./ErrorPopup";
+import { Modal } from "./ui/Modal";
+import axios from "axios";
+import { waitForTransactionReceipt } from "viem/actions";
+import { arbitrumSepolia } from "viem/chains";
+import { OpenloginSessionManager } from "@toruslabs/session-manager";
+
+const ConnectStep = ({ 
+  showSummary = false,
+  onSuccess,
+}: { 
+  showSummary?: boolean,
+  onSuccess: () => void,
+ }) => {
+  const { walletProvider, address: web3PayAddress, selectedEnv } = useWallet();
+  const [randomWallet, setRandomWallet] = useState<IRandomWallet>();
+  const [completedSteps, setCompletedSteps] = useState<ConnectWeb3PayStep[]>(
+    []
+  );
+  const [displayErrorPopup, setDisplayErrorPopup] = useState(false);
+  const [errorStep, setErrorStep] = useState<string | undefined>();
+  const [errorText, setErrorText] = useState("");
+  const [subErrorText, setSubErrorText] = useState("");
+  const [currentStep, setCurrentStep] = useState<ConnectWeb3PayStep>("start");
+  const [stepLoader, setStepLoader] = useState(false);
+
+  const handleStep = async (step: ConnectWeb3PayStep) => {
+    switch (step) {
+      case "start": 
+        await handleCreateAndFundRandomWallet();
+        break;
+      case "connect":
+        await importAccount(randomWallet as IRandomWallet);
+        break;
+      default:
+        break;
+    }
+  }
+  async function handleCreateAndFundRandomWallet() {
+    try {
+      setStepLoader(true);
+      const privateKeyBuf = generatePrivate();
+      const publicKeyBuf = getPublic(privateKeyBuf);
+
+      const privateKey = privateKeyBuf.toString("hex");
+      const publicKey = publicKeyBuf.toString("hex");
+
+      const address = privateKeyToAddress(
+        privateKey.startsWith("0x") ? (privateKey as Hex) : `0x${privateKey}`
+      );
+      await fundAccount(address);
+      setRandomWallet({
+        publicKey,
+        privateKey,
+        address,
+        keyType: "secp256k1",
+      });
+      setCompletedSteps([...completedSteps, "start"]);
+      setCurrentStep("connect");
+    } catch (err: any) {
+      setErrorStep("connect");
+      setDisplayErrorPopup(true);
+      console.error("error while creating random wallet", err);
+      setErrorText("Error while creating or funding wallet");
+    } finally {
+      setStepLoader(false);
+    }
+  }
+
+  async function fundAccount(address: string) {
+    try {
+        setStepLoader(true);
+        const baseUrl = calculateBaseUrl(selectedEnv);
+
+        const resp = await axios.post(`${baseUrl}/api/mint`, {
+          chainId: "421614",
+          toAddress: address,
+        });
+        const { txHash: hash, message } = resp.data;
+        // setTxHash(hash);
+        const publicClient = createPublicClient({
+          chain: arbitrumSepolia,
+          transport: http(
+            "https://arbitrum-sepolia.infura.io/v3/dee726a2930e4573a743a5c8f79942c1"
+          ),
+        });
+        if (hash) {
+          await waitForTransactionReceipt(publicClient, {
+            hash,
+          });
+        } else {
+          throw new Error("Failed to fund test wallet");
+        }
+
+    } catch (error: any) {
+      setErrorText("Error while Funding Wallet");
+      // check if user has enough balance and proceed to next step
+    } finally {
+      setStepLoader(false);
+    }
+  }
+
+  async function importAccount(randWallet: IRandomWallet) {
+    try {
+      setStepLoader(true);
+      if (web3PayAddress) {
+        const { privateKey, publicKey, keyType } = randWallet;
+        let sessionId = OpenloginSessionManager.generateRandomSessionKey();
+        const sessionMgr = new OpenloginSessionManager({ sessionId });
+        sessionId = await sessionMgr.createSession({
+          privateKey,
+          publicKey,
+          keyType,
+        });
+
+        const response = await walletProvider?.request({
+          method: "wallet_importW3aSession",
+          params: {
+            sessionId,
+          },
+        });
+        console.log("Response", response);
+        setCompletedSteps([...completedSteps, "connect"]);
+
+        onSuccess();
+      }
+    } catch (e: unknown) {
+      console.error("error importing account", e);
+      setErrorText("Error while connecting account");
+      throw e;
+    } finally {
+      setStepLoader(true);
+    }
+  }
+
+  async function onRetry() {
+    console.log("onRetry::errorStep", errorStep);
+    if (errorStep === "connect") {
+      await handleCreateAndFundRandomWallet();
+    }
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      {!showSummary && (
+        <>
+          <p className="text-4xl font-bold">Connecting with Web3Pay</p>
+          <p className="text-2xl font-normal mt-2 w-full md:w-[60%] text-center">
+            For demo purposes, a test funded wallet will be used instead of your
+            external EOA wallets
+          </p>
+        </>
+      )}
+      <div
+        className={cn(
+          "mt-16 w-full flex items-center flex-col sm:flex-row justify-center lg:w-[85%] xl:w-[60%]",
+          { "mt-0": showSummary }
+        )}
+      >
+        <ImportFlowCard
+          title="Create your funded wallet on Arbitrum"
+          description="Get a funded test wallet for the demo that will stand in for your external wallets"
+          step="1"
+          isCompleted={completedSteps.includes("start")}
+          isCurrent={currentStep === "start"}
+          logo="arbitrum"
+          resultOpacity
+          resultText={sliceAddress(randomWallet?.address || "")}
+          resultLogo="arbitrum"
+          handleClick={() => handleStep('start')}
+          btnText="Create test wallet"
+          loading={stepLoader}
+          handleCompletedLink={() =>
+            openInNewTab(
+              `https://sepolia.arbiscan.io/address/${randomWallet?.address}`
+            )
+          }
+        />
+        <Image
+          src="/icons/arrow-right.svg"
+          alt="arrow"
+          height={50}
+          width={50}
+          className="rotate-90 my-5 mx-auto block md:hidden"
+        />
+        <Image
+          src="/icons/arrow-white.svg"
+          alt="arrow"
+          height={100}
+          width={100}
+          className="hidden md:block"
+        />
+        <ImportFlowCard
+          title="Connect your wallet to Web3Pay Account"
+          description="Allow your Web3Pay Account to access liquidity from your test wallet."
+          step="2"
+          isCompleted={completedSteps.includes("connect")}
+          isCurrent={currentStep === "connect"}
+          logo="arbitrum"
+          resultOpacity
+          resultText={sliceAddress(randomWallet?.address || "")}
+          resultLogo="arbitrum"
+          handleClick={() => handleStep("connect")}
+          btnText="Connect Test Wallet"
+          loading={stepLoader}
+          handleCompletedLink={() =>
+            openInNewTab(
+              `https://sepolia.arbiscan.io/address/${randomWallet?.address}`
+            )
+          }
+        />
+      </div>
+      <Modal
+        isOpen={displayErrorPopup}
+        onClose={() => setDisplayErrorPopup(false)}
+      >
+        <ErrorPopup
+          handleTryAgain={onRetry}
+          subText={subErrorText}
+          text={errorText}
+        />
+      </Modal>
+    </div>
+  );
+};
+
+export default ConnectStep;
---
+++ src/components/CrossMintingStep.tsx
@@ -0,0 +1,281 @@
+import { useWallet } from "@/context/walletContext";
+import { calculateBaseUrl, cn } from "@/utils/utils";
+import Link from "next/link";
+import Image from "next/image";
+import { bundlerActions, ENTRYPOINT_ADDRESS_V07 } from "permissionless";
+import { useState } from "react";
+import { HiOutlineArrowSmRight } from "react-icons/hi";
+import { encodeFunctionData, Hex, createClient, http } from "viem";
+import { polygonAmoy } from "viem/chains";
+import Button from "./ui/Button";
+import Card from "./ui/Card";
+import GradientButton from "./ui/GradientButton";
+import { erc721Abi } from "@/utils/abis/erc721";
+import ErrorPopup from "./ErrorPopup";
+import { Modal } from "./ui/Modal";
+
+const CrossMintingStep = ({
+  showSummary = false,
+  onSuccess,
+}: {
+  showSummary?: boolean;
+  onSuccess: () => void;
+}) => {
+  const { walletProvider, address: web3PayAddress, selectedEnv } = useWallet();
+  const [displayErrorPopup, setDisplayErrorPopup] = useState(false);
+  const [errorStep, setErrorStep] = useState<string | undefined>();
+  const [errorText, setErrorText] = useState("");
+  const [subErrorText, setSubErrorText] = useState("");
+  const [mintNftState, setMintNftState] = useState({
+    minting: false,
+    mintSuccess: false,
+    mintError: "",
+    userOpHashUrl: "",
+    txHashUrl: "",
+  });
+   
+  async function mintNft() {
+    try {
+      setMintNftState({
+        mintError: "",
+        minting: true,
+        mintSuccess: false,
+        userOpHashUrl: "",
+        txHashUrl: "",
+      });
+      const data = encodeFunctionData({
+        abi: erc721Abi,
+        functionName: "mint",
+        args: [web3PayAddress],
+      });
+
+      const resp = await walletProvider?.request({
+        method: "eth_sendTransaction",
+        params: {
+          from: web3PayAddress,
+          to: "0xd774B6e1880dC36A3E9787Ea514CBFC275d2ba61",
+          data,
+          value: "0",
+        },
+      });
+      if (resp) {
+        // setMintSuccess(true);
+        setMintNftState({
+          mintError: "",
+          minting: false,
+          mintSuccess: true,
+          userOpHashUrl: `https://jiffyscan.xyz/userOpHash/${resp}`,
+          txHashUrl: "",
+        });
+        await waitForMinting(resp);
+      }
+      const nftImageUrl = `${calculateBaseUrl(
+        selectedEnv
+      )}/wallet/nft/0xd774B6e1880dC36A3E9787Ea514CBFC275d2ba61`;
+    } catch (e: unknown) {
+      console.error("error minting nft", e);
+      setErrorStep("mintNft");
+      setErrorText("Error while minting");
+      setDisplayErrorPopup(true);
+      setMintNftState({
+        mintError: "Error while minting",
+        minting: false,
+        mintSuccess: false,
+        userOpHashUrl: "",
+        txHashUrl: "",
+      });
+      throw e;
+    } finally {
+    }
+  }
+
+  async function waitForMinting(hash: Hex) {
+    const bundlerClient = createClient({
+      chain: polygonAmoy,
+      transport: http(
+        "https://rpc-proxy.web3auth.io/?network=80002"
+      ),
+    }).extend(bundlerActions(ENTRYPOINT_ADDRESS_V07));
+
+    // wait for user op hash to be completed
+    const userOperationByHash = await bundlerClient.waitForUserOperationReceipt(
+      {
+        hash,
+        timeout: 1000 * 60 * 3,
+        pollingInterval: 1000 * 3,
+      }
+    );
+    if (userOperationByHash.receipt) {
+      setMintNftState({
+        mintError: "",
+        minting: false,
+        mintSuccess: true,
+        userOpHashUrl: `https://jiffyscan.xyz/userOpHash/${hash}`,
+        txHashUrl: `https://amoy.polygonscan.com/tx/${userOperationByHash.receipt.transactionHash}`,
+      });
+    }
+  }
+
+  async function onRetry() {
+    try {
+      await mintNft();
+    } catch (e: unknown) {
+      console.error("error on retrying mint", e);
+    }
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full">
+      {!showSummary && (
+        <>
+          <p className="text-4xl font-bold">
+            {mintNftState.mintSuccess ? "Cross-Chain Minting" : "Connecting with Web3Pay"}
+          </p>
+          <p className="text-2xl font-normal mt-2 w-full md:w-[60%] text-center">
+            {mintNftState.mintSuccess
+              ? "Next, use funds from your test wallet to mint your first NFT on a different chain."
+              : "For demo purposes, a test funded wallet will be used instead of your external EOA wallets"}
+          </p>
+          {mintNftState.mintSuccess && (
+            <Button
+              onClick={() => onSuccess()}
+              title="View Demo Summary"
+              otherClasses="bg-primary"
+              style={{ marginTop: "24px" }}
+            />
+          )}
+        </>
+      )}
+      <div
+        className={cn("w-full xl:w-[80%] mt-16", {
+          "mt-10": mintNftState.mintSuccess,
+          "mt-0": showSummary,
+        })}
+      >
+        <Card
+          active
+          cardClasses={`${
+            mintNftState.mintSuccess ? "!p-6 md:!px-16 md:!py-10" : "!p-0"
+          } !w-full bg-primary`}
+          rootClasses="!w-full"
+        >
+          <div
+            className={cn("flex flex-col md:flex-row items-start gap-x-16", {
+              "items-center": mintNftState.mintSuccess,
+            })}
+          >
+            {mintNftState.mintSuccess ? (
+              <span className="relative w-full md:w-[50%]">
+                <Image
+                  src={"/images/web3pay-nft.png"}
+                  alt="cross chain nft mint"
+                  height={300}
+                  width={300}
+                  className="w-full h-full md:w-[300px] md:h-[300px]"
+                />
+                <Image
+                  src={`/icons/polygon.svg`}
+                  alt={"polygon"}
+                  height={50}
+                  width={50}
+                  className="absolute -top-3 -right-3"
+                />
+              </span>
+            ) : (
+              <Image
+                src={"/images/cross-chain-nft-mint.png"}
+                alt="cross chain nft mint"
+                height={500}
+                width={400}
+                className="w-full h-full"
+              />
+            )}
+            <div
+              className={cn("flex flex-col w-full p-10 md:pl-0 h-full", {
+                "p-0 mt-6": mintNftState.mintSuccess,
+              })}
+            >
+              <div className="flex items-center gap-x-2 w-fit">
+                <Image
+                  src={`/icons/arbitrum.svg`}
+                  alt={"arbitrum"}
+                  height={30}
+                  width={30}
+                />
+                <HiOutlineArrowSmRight />
+                <Image
+                  src={`/icons/polygon.svg`}
+                  alt={"polygon"}
+                  height={30}
+                  width={30}
+                />
+              </div>
+              <p className="text-2xl font-bold text-white w-full mt-4 break-words">
+                {mintNftState.mintSuccess
+                  ? "MINT SUCCESSFUL!"
+                  : "Mint your first cross-chain NFT with Web3Pay!"}
+              </p>
+              <p
+                className={cn(
+                  "text-lg font-normal text-gray-400 w-full md:w-[70%] mt-2 mb-6 break-words",
+                  {
+                    "md:w-[80%] mb-0": mintNftState.mintSuccess,
+                  }
+                )}
+              >
+                {mintNftState.mintSuccess
+                  ? showSummary
+                    ? "Hooray! You just minted a polygon NFT with Arbitrum tokens! "
+                    : "You just minted a polygon NFT with Arbitrum tokens! It should appear in your wallet in about 5 minutes."
+                  : "Use your Arbitrum Test Tokens to mint an NFT on Polygon, no bridging required."}
+              </p>
+              {mintNftState.mintSuccess && (
+                <Link
+                  target="_blank"
+                  href={mintNftState.txHashUrl || mintNftState.userOpHashUrl}
+                  className="text-lg font-normal text-blue-500 mt-6"
+                >
+                  View transaction on Polygon
+                </Link>
+              )}
+              {/* {mintNftState.mintSuccess && (
+                <Link
+                  href={""}
+                  className="text-lg font-normal text-blue-500 mt-2"
+                >
+                  View transaction on Arbitrum
+                </Link>
+              )} */}
+              {mintNftState.mintSuccess ? (
+                <Button
+                  title="Share your experience on X"
+                  otherClasses="bg-primary max-md:!w-full"
+                  style={{ marginTop: "24px" }}
+                />
+              ) : (
+                <GradientButton
+                  loading={mintNftState.minting}
+                  title="Mint NFT"
+                  handleClick={() => {mintNft()}}
+                  btnClass="max-md:!w-full"
+                />
+              )}
+            </div>
+          </div>
+        </Card>
+      </div>
+      <Modal
+        isOpen={displayErrorPopup}
+        onClose={() => setDisplayErrorPopup(false)}
+      >
+        <ErrorPopup
+          handleTryAgain={onRetry}
+          subText={subErrorText}
+          text={errorText}
+        />
+      </Modal>
+    </div>
+  );
+};
+
+export default CrossMintingStep;
